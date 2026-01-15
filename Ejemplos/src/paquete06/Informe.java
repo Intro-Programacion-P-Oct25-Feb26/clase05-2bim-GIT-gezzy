040db3384c9f36e106c36049dbb002e0569f5e03
GIT-gezzy <jdavichomaldonado@gmail.com>
2026-01-15 11:13:44 -0500

    Creacion paquete06 - Clase Informe - metodo imprimir

---
+++ Ejemplos/src/paquete06/Informe.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06;
+
+/**
+ *
+ * @author jdavi
+ */
+public class Informe {
+
+    public static void imprimir(int a, int b, int sum, int mult) {
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                a, b, sum, a, b, mult);
+
+    }
+
+}
